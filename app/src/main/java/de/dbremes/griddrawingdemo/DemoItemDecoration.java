3d6abdabc27d8795945654ee9685ea14e431a55f
ghdbre <ghdbre@snafu.de>
2018-02-11 16:52:58 +0100

    Using GridLayoutManager.findFirstVisibleItemPosition() to keep line under same row when scrolling

--- app/src/main/java/de/dbremes/griddrawingdemo/DemoItemDecoration.java
+++ app/src/main/java/de/dbremes/griddrawingdemo/DemoItemDecoration.java
@@ -3,10 +3,13 @@ package de.dbremes.griddrawingdemo;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 
 public class DemoItemDecoration extends RecyclerView.ItemDecoration {
+    final String TAG = DemoItemDecoration.class.getSimpleName();
     final Paint mPaint;
 
     public DemoItemDecoration() {
@@ -18,7 +21,11 @@ public class DemoItemDecoration extends RecyclerView.ItemDecoration {
     @Override
     public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
         float startX = parent.getPaddingLeft();
-        View childToDrawUnder = parent.getChildAt(150);
+        GridLayoutManager lm = (GridLayoutManager)parent.getLayoutManager();
+        int firstVisibleItemPosition = lm.findFirstVisibleItemPosition();
+        Log.v(TAG, "firstVisibleItemPosition=" + firstVisibleItemPosition);
+        int childToDrawUnderPosition = 150 - firstVisibleItemPosition;
+        View childToDrawUnder = parent.getChildAt(childToDrawUnderPosition);
         float startY = Float.NaN;
         if (childToDrawUnder != null) {
             startY = childToDrawUnder.getBottom();
